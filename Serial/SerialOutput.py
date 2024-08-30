e1a26deea826a66ee0e63a4d035897d7bbb11fea
connor <connor@connor>
2024-08-30 14:02:48 +0100

    gpio serial test

---
+++ Serial/SerialOutput.py
@@ -0,0 +1,16 @@
+import serial
+
+ser = serial.Serial('/dev/serial0', 9600)
+
+def send_data():
+    while True:
+        data = "Test\n"
+        ser.write(data.encode())
+        time.sleep(0.1)
+
+try:
+    send_data()
+except KeyboardInterrupt:
+    print('Interrupted')
+finally:
+    ser.close()
