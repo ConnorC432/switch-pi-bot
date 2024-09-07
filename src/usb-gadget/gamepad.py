d55882a7bc8561f96bf3041ebf1c52961505ba0f
connor <connor@connor>
2024-09-07 09:51:38 +0100

    changed variables in gamepad class

--- src/usb-gadget/gamepad.py
+++ src/usb-gadget/gamepad.py
@@ -48,12 +48,19 @@ class Gamepad:
         self.device_path = device_path
 
     #Write to USB Gadget block device
-    def _write(self, write_time, data):
+    def _write(self, data):
         with open(self.device_path, "wb") as device:
             try:
                 device.write(data)
-                time.sleep(write_time)
+
+            except Exception as e:
+                print(f"Cant write to block device: {e}")
+
+    def _clear(self):
+        with open(self.device_path, "wb") as device:
+            try:
                 device.write(self.DEFAULT_STATE)
+                return self.DEFAULT_STATE
 
             except Exception as e:
                 print(f"Cant write to block device: {e}")
@@ -82,7 +89,10 @@ class Gamepad:
         print(f"Sending to HID Device: {report_hex}")
 
         #Write hex to block device
-        self._write(hold_time, report_hex)
+        self._write(report_hex)
+        time.sleep(hold_time)
+        report_hex = self._clear()
+        print(f"Report Hex Array: {report_hex}")
 
     def press_dpad(self, hold_time, direction):
         report_hex = bytearray(self.DEFAULT_STATE)
@@ -93,7 +103,10 @@ class Gamepad:
 
             print(f"Sending to HID Device: {report_hex}")
 
-            self._write(hold_time, report_hex)
+            self._write(report_hex)
+            time.sleep(hold_time)
+            report_hex = self._clear()
+            print(f"Report Hex Array: {report_hex}")
 
     def move_left_stick(self, hold_time, direction):
         report_hex = bytearray(self.DEFAULT_STATE)
@@ -107,7 +120,10 @@ class Gamepad:
 
             print(f"Sending to HID Device: {report_hex}")
 
-            self._write(hold_time, report_hex)
+            self._write(report_hex)
+            time.sleep(hold_time)
+            report_hex = self._clear()
+            print(f"Report Hex Array: {report_hex}")
 
     def move_right_stick(self, hold_time, direction):
         report_hex = bytearray(self.DEFAULT_STATE)
@@ -121,7 +137,10 @@ class Gamepad:
 
             print(f"Sending to HID Device: {report_hex}")
 
-            self._write(hold_time, report_hex)
+            self._write(report_hex)
+            time.sleep(hold_time)
+            report_hex = self._clear()
+            print(f"Report Hex Array: {report_hex}")
 
 
 if __name__ == "__main__":
