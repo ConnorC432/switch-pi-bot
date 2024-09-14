6a4d5fb53c567a9b62d68ba7110346fb29fd9add
connor <connor@connor>
2024-09-14 19:34:25 +0100

    custom pi-gen

--- pi-gen/00-run-chroot.sh
+++ pi-gen/00-run-chroot.sh
@@ -1,7 +1,26 @@
+mkdir -p ${ROOTFS_DIR}/proc
+mkdir -p ${ROOTFS_DIR}/dev
+mkdir -p ${ROOTFS_DIR}/sys
+mkdir -p ${ROOTFS_DIR}/run
+mkdir -p ${ROOTFS_DIR}/tmp
+
+mount -o bind /proc ${ROOTFS_DIR}/proc
+mount -o bind /dev ${ROOTFS_DIR}/dev
+mount -o bind /sys ${ROOTFS_DIR}/sys
+mount -o bind /run ${ROOTFS_DIR}/run
+mount -o bind /tmp ${ROOTFS_DIR}/tmp
+
 apt update
 apt upgrade -y
 apt purge nodejs npm nodejs-legacy libnode72
 apt autoremove
 curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
 bash nodesource_setup.sh
+apt-get install -fy nodejs/nodistro
 rm nodesource_setup.sh
+
+umount ${ROOTFS_DIR}/proc
+umount ${ROOTFS_DIR}/dev
+umount ${ROOTFS_DIR}/sys
+umount ${ROOTFS_DIR}/run
+umount ${ROOTFS_DIR}/tmp
