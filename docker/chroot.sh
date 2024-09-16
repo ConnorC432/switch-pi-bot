37241c97222517494ed5f955c73026901ff8af49
ConnorC432 <connor.chetham@gmail.com>
2024-09-16 11:34:00 +0100

    docker tweaked

--- docker/chroot.sh
+++ docker/chroot.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-wget -v -O pi-bot.img.xz https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64-lite.img.xz
-xz --decompress -v pi-bot.img.xz
-
-IMG_FILE="pi-bot.img"
-MOUNT_DIR="/mnt/chroot"
-LOOP_DEVICE=$(losetup -f)
-
-mkdir -vp $MOUNT_DIR
-
-losetup -v "$LOOP_DEVICE" $IMG_FILE
-kpartx -av "$LOOP_DEVICE"
-
-ROOT_PARTITION=$(ls /dev/mapper/loop* | grep -E '^/dev/loop[0-9]+p2$')
-if [ -z "$ROOT_PARTITION" ]; then
+if [ -z "$PI_ROOT" ]; then
 	exit 1
 fi
 
-mount "$ROOT_PARTITION" $MOUNT_DIR
+mkdir -vp /mnt/chroot
+MOUNT_DIR=/mnt/chroot
+mount "$PI_ROOT" "$MOUNT_DIR"
 
 mount -v --bind /dev $MOUNT_DIR/dev
 mount -v --bind /dev/pts $MOUNT_DIR/dev/pts
@@ -34,8 +23,6 @@ exit_chroot() {
 	umount $MOUNT_DIR/sys
 	umount $MOUNT_DIR/src
 	losetup -vd "$LOOP_DEVICE"
-
-	cp -v $IMG_FILE /build/$IMG_FILE
 }
 
 trap exit_chroot EXIT INT
