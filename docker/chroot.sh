1aa40cf8a7a5c1eacf3f00503f4d277e2c475c74
ConnorC432 <connor.chetham@gmail.com>
2024-09-16 10:55:53 +0100

    updated docker chroot script

--- docker/chroot.sh
+++ docker/chroot.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-wget -O pi-bot.img https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64-lite.img.xz
+wget -v -O pi-bot.img.xz https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64-lite.img.xz
 xz --decompress -v pi-bot.img.xz
 
 IMG_FILE="pi-bot.img"
 MOUNT_DIR="/mnt/chroot"
 LOOP_DEVICE=$(losetup -f)
 
-mkdir -p $MOUNT_DIR
+mkdir -vp $MOUNT_DIR
 
-losetup "$LOOP_DEVICE" $IMG_FILE
+losetup -v "$LOOP_DEVICE" $IMG_FILE
 kpartx -av "$LOOP_DEVICE"
 
 ROOT_PARTITION=$(ls /dev/mapper/loop* | grep -E '^/dev/loop[0-9]+p2$')
@@ -19,11 +19,11 @@ fi
 
 mount "$ROOT_PARTITION" $MOUNT_DIR
 
-mount --bind /dev $MOUNT_DIR/dev
-mount --bind /dev/pts $MOUNT_DIR/dev/pts
-mount --bind /proc $MOUNT_DIR/proc
-mount --bind /sys $MOUNT_DIR/sys
-mount --bind /src $MOUNT_DIR/src
+mount -v --bind /dev $MOUNT_DIR/dev
+mount -v --bind /dev/pts $MOUNT_DIR/dev/pts
+mount -v --bind /proc $MOUNT_DIR/proc
+mount -v --bind /sys $MOUNT_DIR/sys
+mount -v --bind /src $MOUNT_DIR/src
 
 chroot $MOUNT_DIR /bin/bash -c "bash /usr/local/bin/install.sh"
 
@@ -33,9 +33,9 @@ exit_chroot() {
 	umount $MOUNT_DIR/proc
 	umount $MOUNT_DIR/sys
 	umount $MOUNT_DIR/src
-	losetup -d "$LOOP_DEVICE"
+	losetup -vd "$LOOP_DEVICE"
 
-	cp $IMG_FILE /build/$IMG_FILE
+	cp -v $IMG_FILE /build/$IMG_FILE
 }
 
 trap exit_chroot EXIT INT
