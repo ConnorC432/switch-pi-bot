4c2366a1ad1b72890f9c582071b060249f80274d
ConnorC432 <connor.chetham@gmail.com>
2024-09-17 10:16:56 +0100

    backend service fixed

--- docker/chroot.sh
+++ docker/chroot.sh
@@ -40,10 +40,10 @@ EOF
 	ln -sf /opt/switch-pi-bot/firstboot.sh /etc/init.d/firstboot.sh
 	chown -vR pibot:pibot /opt/switch-pi-bot
 
-	# Create .venv for python backend
-	python3 -m venv /opt/switch-pi-bot/src/switch-control/.venv
-	/opt/switch-pi-bot/src/switch-control/.venv/bin/pip install --upgrade pip
-	/opt/switch-pi-bot/src/switch-control/.venv/bin/pip install -r /opt/switch-pi-bot/requirements.txt
+	# Create venv for python backend
+	python3 -m venv /opt/switch-pi-bot/src/switch-control/venv
+	/opt/switch-pi-bot/src/switch-control/venv/bin/pip install --upgrade pip
+	/opt/switch-pi-bot/src/switch-control/venv/bin/pip install -r /opt/switch-pi-bot/requirements.txt
 
 	# NPM Build
 	npm --prefix /opt/switch-pi-bot/src/webui install
