e76e2027f1fb213e44b6c86ab5f916ca173c73e5
ConnorC432 <connor.chetham@gmail.com>
2024-09-17 09:40:57 +0100

    changed first boot script service

--- firstboot.sh
+++ firstboot.sh
@@ -4,5 +4,3 @@ ln -sf /opt/switch-pi-bot/nginx /etc/nginx/sites-enabled/pibot
 systemctl restart nginx
 
 sed -i 's/^dtoverlay=dwc2,dr_mode=host/dr_mode=peripheral: \[cm5\]/' /boot/firmware/config.txt
-
-rm /etc/init.d/firstboot.sh
