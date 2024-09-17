4f6451237234a4b8e17a17f6125eadeded0baa4e
ConnorC432 <connor.chetham@gmail.com>
2024-09-17 09:50:34 +0100

    corrected firstboot sed

--- firstboot.sh
+++ firstboot.sh
@@ -3,4 +3,4 @@
 ln -sf /opt/switch-pi-bot/nginx /etc/nginx/sites-enabled/pibot
 systemctl restart nginx
 
-sed -i 's/^dtoverlay=dwc2,dr_mode=host/dr_mode=peripheral: \[cm5\]/' /boot/firmware/config.txt
+sed -i 's/^dtoverlay=dwc2,dr_mode=host/dtoverlay=dwc2,dr_mode=peripheral/' /boot/firmware/config.txt
