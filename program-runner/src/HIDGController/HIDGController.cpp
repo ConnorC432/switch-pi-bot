f7420dd668789b94af7aa8c5835abb9a7786a7c5
ConnorC432 <connor.chetham@gmail.com>
2025-09-17 13:05:56 +0100

    initial commit

---
+++ program-runner/src/HIDGController/HIDGController.cpp
@@ -0,0 +1,8 @@
+//
+// Created by connor on 17/09/2025.
+//
+
+#include "HIDGController.h"
+
+namespace ProgramWorker {
+} // ProgramWorker
---
+++ program-runner/src/HIDGController/HIDGController.h
@@ -0,0 +1,13 @@
+t//
+// Created by connor on 17/09/2025.
+//
+
+#ifndef SWITCH_BOT_TEMP_HIDGCONTROLLER_H
+#define SWITCH_BOT_TEMP_HIDGCONTROLLER_H
+
+namespace ProgramWorker {
+    class HIDGController {
+    };
+} // ProgramWorker
+
+#endif //SWITCH_BOT_TEMP_HIDGCONTROLLER_H
